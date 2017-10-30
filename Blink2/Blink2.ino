5e2931875d516671e33fc35379be446f1ab2f5af
Jake Mannix <jake.mannix@gmail.com>
2017-10-30 12:51:56 -0700

    fix missing }

--- Blink2/Blink2.ino
+++ Blink2/Blink2.ino
@@ -22,3 +22,4 @@ void loop() {
   delay(200);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
+}
